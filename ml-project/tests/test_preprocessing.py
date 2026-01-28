f3e1cda46fa5a2117b7146b451d99f3277b915bf
Michał Zarzycki <michalzzarzycki@gmail.com>
2026-01-28 12:53:59 +0100

    Remove ml-project directory

--- ml-project/tests/test_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from src.preprocessing import Preprocessor
-
-def test_scaler_shape():
-    X = np.array([[1, 2], [3, 4]])
-    pre = Preprocessor()
-    X_scaled = pre.fit_transform(X)
-
-    assert X.shape == X_scaled.shape
